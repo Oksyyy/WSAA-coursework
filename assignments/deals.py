d528ed6aa00bb84897d7b81a1eb04c13e036d766
Oksana Abrosimova <abricosova.oxi@gmail.com>
2026-04-10 08:53:14 +0100

    Completed assignment 1

--- assignments/deals.py
+++ assignments/deals.py
@@ -1,10 +1,15 @@
 import requests
 import json
 
+# Shuffle the deck - get the deck id
 url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
 response = requests.get(url)
-## 'deck_id': 'ebn1y89scli4'
-cards = "https://deckofcardsapi.com/api/deck/ebn1y89scli4/draw/?count=2"
-deal_1 = requests.get(cards)
-data = deal_1.json()
-print(json.dumps(data, indent=2))
+
+print(json.dumps(response.json(), indent=2))
+
+# 'deck_id': 'ebn1y89scli4'
+deck_id = response.json()['deck_id']
+#cards = f"https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count=2"
+#deal_1 = requests.get(cards)
+#data = deal_1.json()
+#print(json.dumps(data, indent=2))
