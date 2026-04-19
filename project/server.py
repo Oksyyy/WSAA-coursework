bbd26caaa47b42eb5972204900541ef9bc112838
Oksana Abrosimova <abricosova.oxi@gmail.com>
2026-04-19 10:50:40 +0100

    Set up database schema and implemented DAO for CRUD operations

--- project/server.py
+++ project/server.py
@@ -14,51 +14,57 @@ def index():
 # curl http://127.0.0.1:5000/providers
 
 @app.route('/providers', methods=['GET'])
-def getall():
-        return jsonify({"message": "get all"})
+def get_providers():
+        return jsonify({"message": "get all providers"})
 
 # Find by id
 # curl http://127.0.0.1:5000/providers/1
 
 @app.route('/providers/<int:id>', methods=['GET'])
-def findbyid(id):
-        return "find by id"
+def provider_by_id(id):
+        return "find provider by id"
 
 # Create
 #curl -X POST -d "{\"name\":\"test\", \"email\":\"some@gmail.com\", \"phone\":\"01234567\", \"price\":70}" http://127.0.0.1:5000/providers
 @app.route('/providers', methods=['POST'])
-def create():
-        return jsonify({"message": "create"})
+def create_provider():
+        return jsonify({"message": "create provider"})
 
 # update
 # curl -X PUT -d "{\"name\":\"test\", \"email\":\"some@gmail.com\", http://127.0.0.1:5000/providers/1
 
 @app.route('/providers/<int:id>', methods=['PUT'])
-def update(id):
+def update_provider(id):
         jsonstring = request.json
-        return f"update {id} {jsonstring}"
+        return f"update provider {id} {jsonstring}"
 
 # Delete
 # curl -X DELETE  http://127.0.0.1:5000/providers/1
 
 @app.route('/providers/<int:id>', methods=['DELETE'])
-def delete(id):
-        return f"delete {id}"
+def delete_provider(id):
+        return f"delete provider {id}"
 
 # SERVECES
 
-# Get all
+# Get all services
 # curl http://127.0.0.1:5000/services
 
 @app.route('/services', methods=['GET'])
-def getall():
-        return jsonify({"message": "get all"})
+def get_services():
+        return jsonify({"message": "get all services"})
 
-# Create
+# Create a service
 #curl -X POST -d "{\"name\":\"test\", \"email\":\"some@gmail.com\", \"phone\":\"01234567\", \"price\":70}" http://127.0.0.1:5000/providers
-@app.route('/providers', methods=['POST'])
+@app.route('/services', methods=['POST'])
 def create():
-        return jsonify({"message": "create"})
+        return jsonify({"message": "create a service"})
+
+# Get service and service providers
+# curl http://127.0.0.1:5000//providers/service/1
+@app.route('/providers/service/<int:service_id>', methods=['GET'])
+def get_by_service(service_id):
+    return jsonify({"message": f"providers for service {service_id}"})
 
 
 if __name__ == "__main__":
